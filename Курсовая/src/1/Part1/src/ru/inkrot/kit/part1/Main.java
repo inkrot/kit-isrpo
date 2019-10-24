e58a39cebf26f911dacca5035fd1ddd6b6020f9d
Islam Zaripov <islam.zar@yandex.ru>
2019-10-25 01:06:15 +0300

    Part1

--- Курсовая/src/1/Part1/src/ru/inkrot/kit/part1/Main.java
+++ Курсовая/src/1/Part1/src/ru/inkrot/kit/part1/Main.java
@@ -7,48 +7,52 @@ import java.awt.*;
 public class Main extends JFrame {
 
     private JPanel valuesPanel;
-    private JLabel label1, label2;
-    private JTextField xStart1, xEnd1;
-    private JTextField xStart2, xEnd2;
+    private JTextField xStartField, xEndField, nField;
     private JButton apply;
     private ChartsPanel chartsPanel;
 
-    private static Dimension screenSize;
-    public static int SCREEN_WIDTH;
-    public static int SCREEN_HEIGHT;
-
     private Font font = new Font("Arial", 0, 16);
+
+    private Dimension screenSize;
 
     Main() {
         setSize(900, 800);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLayout(null);
         initUI();
         setVisible(true);
+        repaint();
     }
 
     private void initUI() {
         valuesPanel = new JPanel();
-        valuesPanel.setLayout(new GridLayout(3,2, 3, 10));
+        valuesPanel.setLayout(new GridLayout(4,2, 3, 3));
         valuesPanel.setBackground(Color.GRAY);
         valuesPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
-        valuesPanel.setBounds(10, 10, 300, 120);
+        valuesPanel.setBounds(11, 11, 250, 130);
         add(valuesPanel);
 
-        valuesPanel.add(label1 = newLabel("X (нач):"));
-        valuesPanel.add(xStart1 = newField());
+        valuesPanel.add(newLabel("X_нач:"));
+        valuesPanel.add(xStartField = newField());
 
-        valuesPanel.add(label1 = newLabel("X (кон):"));
-        valuesPanel.add(xEnd1 = newField());
+        valuesPanel.add(newLabel("X_кон:"));
+        valuesPanel.add(xEndField = newField());
 
-        JButton emptyButton = new JButton();
-        emptyButton.setVisible(false);
-        valuesPanel.add(emptyButton);
+        valuesPanel.add(newLabel("n_max:"));
+        valuesPanel.add(xEndField = newField());
+
+        valuesPanel.add(newEmptyComponent());
 
         valuesPanel.add(apply = new JButton("Применить"));
+        apply.setFont(font);
         apply.setFocusPainted(false);
+
+        chartsPanel = new ChartsPanel();
+        chartsPanel.setBounds(10, 10, getWidth() - 10, getHeight() - 10);
+        add(chartsPanel);
     }
 
     private JLabel newLabel(String text) {
@@ -64,18 +68,74 @@ public class Main extends JFrame {
         return field;
     }
 
+    private JComponent newEmptyComponent() {
+        JComponent component = new JButton();
+        component.setVisible(false);
+        return component;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        chartsPanel.setSize(Main.this.getWidth() - 35, Main.this.getHeight() - 58);
+    }
+
     public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        SCREEN_WIDTH = (int) screenSize.getWidth();
-        SCREEN_HEIGHT = (int) screenSize.getHeight();
         new Main();
     }
 
-    class ChartsPanel extends JFrame {
+    class ChartsPanel extends JPanel {
+
+        int rangeMin = -15;
+        int rangeMax = 15;
 
         ChartsPanel() {
 
+        }
+
+        private int map(int value, int valueRangeMin, int valueRangeMax) {
+            double fromRange = valueRangeMax - valueRangeMin;
+            double toRange = rangeMax - rangeMin;
+            double scaleFactor = toRange / fromRange;
+            double tmpValue = value - valueRangeMin;
+            tmpValue *= scaleFactor;
+            return (int) (tmpValue + rangeMin);
+        }
+
+        private int rX(double xCoord) {
+            return (int) (getWidth() / 2 + xCoord);
+        }
+
+        private int rY(double yCoord) {
+            return (int) (getHeight() / 2 + yCoord);
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            int w = getWidth(), h = getHeight();
+
+            g.setColor(Color.LIGHT_GRAY);
+            g.fillRect(0, 0, w, h);
+
+            g.setColor(Color.BLUE);
+            ((Graphics2D) g).setStroke(new BasicStroke(1));
+            g.drawLine(w / 2, 40, w / 2, h - 40);
+            g.drawLine(40, h / 2, w - 40, h / 2);
+
+            g.setColor(Color.RED);
+            ((Graphics2D) g).setStroke(new BasicStroke(1));
+            double xStart = -100, xEnd = 100, dx = 0.3;
+            int rMaxX = w / 2, rMaxY = h / 2;
+            System.out.println(rMaxX - 80);
+            for (double x = xStart + dx; x <= xEnd; x += dx) {
+                g.drawLine(
+                        map(rX(x - dx), 0, rMaxX),
+                        map(rY(Math.atan(x - dx)), 0, rMaxY),
+                        map(rX(x), 0, rMaxX),
+                        map(rY(Math.atan(x)), 0, rMaxY)
+                );
+            }
         }
     }
 }
