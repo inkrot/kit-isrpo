4f6aae87b63187a6a337b13fb49eaa4e923a582e
Islam Zaripov <islam.zar@yandex.ru>
2019-10-30 10:35:03 +0300

    Part3

--- Курсовая/src/3/Part3/src/ru/inkrot/kit/part3/Main.java
+++ Курсовая/src/3/Part3/src/ru/inkrot/kit/part3/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
             //DataBase.connect();
             Parent root = FXMLLoader.load(getClass().getResource("view/main.fxml"));
             stage.setTitle("Салон продажи сотовых телефонов");
-            stage.setScene(new Scene(root, 600, 400));
+            stage.setScene(new Scene(root, 790, 600));
             stage.setResizable(false);
             stage.show();
         } catch (Exception e) {
